f75a191f3e740e306057cfb8aa5b70b53873167b
NickolasAcosta <NickAcosta@outlook.com>
2016-01-09 13:30:57 -0600

    removed usings

--- QueueDodge/Models/Representation.cs
+++ QueueDodge/Models/Representation.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace QueueDodge.Models
+﻿namespace QueueDodge.Models
 {
     public class Representation <T>
     {
--- QueueDodge/Services/RegionService.cs
+++ QueueDodge/Services/RegionService.cs
@@ -1,9 +1,6 @@
 ﻿using QueueDodge.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QueueDodge.Services
 {
