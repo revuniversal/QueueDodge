93c892279d798d282f11f1a13df051fd36fcfd06
NickAcosta <NickAcosta@outlook.com>
2016-02-24 00:00:59 -0600

    task runner stuff

--- QueueDodge.Site/gulpfile.js
+++ QueueDodge.Site/gulpfile.js
@@ -1,25 +1,25 @@
-﻿// Sass configuration
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var markdown = require('gulp-markdown');
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     gulp.src('./wwwroot/app/styles/style.scss')
         .pipe(sass())
-        .pipe(gulp.dest(function(f) {
-            return f.base;
-        }))
+        .pipe(gulp.dest(function (f) {
+        return f.base;
+    }));
 });
 
-gulp.task('markdown', function() {
+gulp.task('markdown', function () {
     return gulp.src('**/*.md')
         .pipe(markdown())
-        .pipe(gulp.dest(function(f) {
-            return f.base;
-        }));
+        .pipe(gulp.dest(function (f) {
+        return f.base;
+    }));
 });
 
-gulp.task('default', function() {
-     gulp.watch('./wwwroot/app/styles/style.scss', ['sass']);
-     gulp.watch('**/*.md', ['markdown']);
-})
+gulp.task('default', function () {
+    gulp.watch('./wwwroot/app/styles/style.scss', ['sass']);
+    gulp.watch('**/*.md', ['markdown']);
+    gulp.watch('./wwwroot/**/*.ts', ['typescript']);
+});
