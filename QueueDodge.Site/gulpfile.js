f37f3b7230c678136bc87f66c606319b90c805e8
NickAcosta <NickAcosta@outlook.com>
2016-02-28 03:27:52 -0600

    Gulp task for dependencies

--- QueueDodge.Site/gulpfile.js
+++ QueueDodge.Site/gulpfile.js
@@ -2,24 +2,38 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var markdown = require('gulp-markdown');
 
+gulp.task('dependencies', function () {
+    gulp.src([
+        './node_modules/jquery/dist/jquery.js',
+        './node_modules/bootstrap/dist/js/bootstrap.js',
+        './node_modules/angular2/bundles/angular2-polyfills.js',
+        './node_modules/systemjs/dist/system.src.js',
+        './node_modules/rxjs/bundles/Rx.js',
+        './node_modules/angular2/bundles/angular2.dev.js',
+        './node_modules/angular2/bundles/router.dev.js',
+        './node_modules/angular2/bundles/http.dev.js',
+        './node_modules/bootstrap/dist/css/bootstrap.css'
+    ])
+        .pipe(gulp.dest('./wwwroot/vendor/'));
+});
+
 gulp.task('sass', function () {
     gulp.src('./wwwroot/app/styles/style.scss')
         .pipe(sass())
         .pipe(gulp.dest(function (f) {
-        return f.base;
-    }));
+            return f.base;
+        }));
 });
 
 gulp.task('markdown', function () {
     return gulp.src('**/*.md')
         .pipe(markdown())
         .pipe(gulp.dest(function (f) {
-        return f.base;
-    }));
+            return f.base;
+        }));
 });
 
 gulp.task('default', function () {
     gulp.watch('./wwwroot/app/styles/style.scss', ['sass']);
     gulp.watch('**/*.md', ['markdown']);
-    gulp.watch('./wwwroot/**/*.ts', ['typescript']);
 });
